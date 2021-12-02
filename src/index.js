b522231454ac1a21e21e5188c755ccf9798519fa
NikitaMelnychencko <hunter6059@gmail.com>
2021-12-03 01:08:55 +0200

    add_contand
    add_contand

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import './index.css';
+import './style/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
